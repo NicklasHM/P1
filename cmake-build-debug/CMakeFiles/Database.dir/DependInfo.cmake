755d49f82f93c5d391acbe31bd548b718ed79c0a
FrederikSkipper <RK23KN@student.aau.dk>
2024-12-03 00:00:59 +0100

    DATABASE VIRKER!

---
+++ cmake-build-debug/CMakeFiles/Database.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/frederikskipper-andersen/CLionProjects/P1AGAIN/src/Database/database.c" "CMakeFiles/Database.dir/src/Database/database.c.o" "gcc" "CMakeFiles/Database.dir/src/Database/database.c.o.d"
+  "/Users/frederikskipper-andersen/CLionProjects/P1AGAIN/src/Database/simulateLedighed.c" "CMakeFiles/Database.dir/src/Database/simulateLedighed.c.o" "gcc" "CMakeFiles/Database.dir/src/Database/simulateLedighed.c.o.d"
+  "/Users/frederikskipper-andersen/CLionProjects/P1AGAIN/src/Database/updateLedighed.c" "CMakeFiles/Database.dir/src/Database/updateLedighed.c.o" "gcc" "CMakeFiles/Database.dir/src/Database/updateLedighed.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
