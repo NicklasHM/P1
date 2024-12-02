e326a8f862ec5c7503c99eb3e56915901e494728
FrederikSkipper <RK23KN@student.aau.dk>
2024-12-03 00:05:13 +0100

    DATABASE VIRKER!

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include "functions/scan_nmrplade.h"
 #include "functions/beregn_p_plads.h"
-// tjekker branch
+// tjekker branch1
 // Hovedprogram
 int main() {
     PriorityQueue afstandHeap = { .size = 0 };
