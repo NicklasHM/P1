4b44a59fc370437c5567de2b195a2592fbb7a752
Nick <153918374+nicklas-mikkelsen@users.noreply.github.com>
2024-12-19 15:33:13 +0100

    Fjernet engelske kommentarer. FÆRDIGT PRODUKT

--- src/functions/scan_nmrplade.c
+++ src/functions/scan_nmrplade.c
@@ -6,11 +6,11 @@ int line_exists(FILE *file, const char *input) {
     char line[256];
     rewind(file);
     while (fgets(line, sizeof(line), file) != NULL) {
-        line[strcspn(line, "\r\n")] = 0; // Remove newline and carriage return
+        line[strcspn(line, "\r\n")] = 0; // Fjern linjeskift
         if (strncmp(line, input, strlen(input)) == 0 &&
             (line[strlen(input)] == ' ' || line[strlen(input)] == '\0')) {
             strncpy(input, line, 256);
-            return 1; // Line exists
+            return 1; // Linjen eksisterer
             }
     }
     return 0; // Linjen eksisterer ikke
@@ -18,7 +18,7 @@ int line_exists(FILE *file, const char *input) {
 
 void write_to_file(FILE *file, const char *input, int præferenceHandicap, int præferenceEl, int præferenceKriterium) {
     if (input == NULL || input[0] == '\0') {
-        return;  // Do nothing if the input is empty or just a newline
+        return;  // Gør intet, hvis linjen er tom.
     }
     if (line_exists(file, input)) {
         printf("The numberplate '%s' is already registered.\n", input);
